35d361ac856e89afc0818b2896e57f167165e244
jcyang <34616545+jcyang34@users.noreply.github.com>
2023-08-01 13:53:15 -0400

    Create volcano.R

---
+++ volcano.R
@@ -0,0 +1,16 @@
+volcano_p <- ggplot(data=top_DEs_all, aes(x=avg_logFC, y=-log10(p_val_adj), col=cell_type,shape=regions,label=delabel,alpha=DE)) +
+  geom_point(size=3) +
+ theme_linedraw() +
+  geom_text_repel( max.overlaps =30) +
+  scale_color_manual(values=celltype_level0_color_palette) +
+  scale_alpha_discrete(range = c(0.1, 0.9))+
+  geom_vline(xintercept=c(-2, 2), col=c("blue","red"), linetype="dotted",size=1) +
+  annotate(geom="text", x=3, y=30, label="HIV+",
+             color="red",size=5) + 
+  annotate(geom="text", x=-3, y=30, label="CTR",
+           color="blue",size=5)+
+  geom_hline(yintercept=-log10(1e-10), col="black", linetype="dotted",size=1) + 
+  theme(axis.text=element_text(size=12),
+         axis.title=element_text(size=14,face="bold"),
+        legend.title=element_text(size=14,face = "bold"), 
+        legend.text=element_text(size=12))
